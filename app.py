928d8f1823b1240a9437602fde275ec4b87ba128
mmorrisj <34495761+mmorrisj@users.noreply.github.com>
2024-06-06 18:53:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,24 @@ def children_dashboard():
 def logout():
     session.clear()
     return redirect(url_for('login'))
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = 'parent'
+
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        conn = get_db_connection()
+        conn.execute('INSERT INTO users (name, role, password) VALUES (?, ?, ?)', (username, role, hashed_password))
+        conn.commit()
+        conn.close()
+        
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
