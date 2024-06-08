84ffaecb9d2ccea4e921fedf8f5e8938fb9c5d38
mmorrisj <34495761+mmorrisj@users.noreply.github.com>
2024-06-08 00:47:23 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -27,6 +27,26 @@ CREATE TABLE completed_chores (
     FOREIGN KEY (chore_id) REFERENCES chores(id)
 );
 
+DROP TABLE IF EXISTS expenses;
+CREATE TABLE expenses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    preset_amount REAL NOT NULL,
+    type TEXT NOT NULL
+);
+
+-- Completed expenses table
+DROP TABLE IF EXISTS completed_expenses;
+CREATE TABLE completed_expenses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    expense_id INTEGER,
+    amount_deducted REAL NOT NULL,
+    date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (expense_id) REFERENCES expenses(id)
+);
+
 
 INSERT INTO chores (name, preset_amount, type, time_of_day) VALUES ('Act of Kindness', 1, 'preset', 'Any');
 INSERT INTO chores (name, preset_amount, type, time_of_day) VALUES ('5 Minute Helpfulness', 5, 'preset', 'Any');
