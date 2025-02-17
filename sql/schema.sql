d19370d7afa89aa9b783147681f61fe38bc107d7
mmorrisj <34495761+mmorrisj@users.noreply.github.com>
2025-02-17 14:48:44 +0000

    updated schema and test

--- sql/schema.sql
+++ sql/schema.sql
@@ -89,48 +89,48 @@ CREATE TABLE IF NOT EXISTS completed_open_chores (
     FOREIGN KEY (open_chore_id) REFERENCES open_chores(id)
 );
 
-INSERT INTO users (name, role, password) VALUES ('Virginia', 'child', 'virginia');
-INSERT INTO users (name, role, password) VALUES ('Evelyn', 'child', 'evelyn');
-INSERT INTO users (name, role, password) VALUES ('Lucy', 'child', 'lucy');
-INSERT INTO users (name, role, password) VALUES ('Parent', 'parent', 'parent')
+INSERT OR IGNORE INTO users (name, role, password) VALUES ('Virginia', 'child', 'virginia');
+INSERT OR IGNORE INTO users (name, role, password) VALUES ('Evelyn', 'child', 'evelyn');
+INSERT OR IGNORE INTO users (name, role, password) VALUES ('Lucy', 'child', 'lucy');
+INSERT OR IGNORE INTO users (name, role, password) VALUES ('Parent', 'parent', 'parent')
 
-INSERT INTO behaviors (name, amount, type) VALUES ('Act of Kindness', .25, 'good');
-INSERT INTO behaviors (name, amount, type) VALUES ('5 Minute Helpfulness', 1, 'good');
-INSERT INTO behaviors (name, amount, type) VALUES ('10 Minute Helpfulness', 2, 'good');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('Act of Kindness', .25, 'good');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('5 Minute Helpfulness', 1, 'good');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('10 Minute Helpfulness', 2, 'good');
 
-INSERT INTO behaviors (name, amount, type) VALUES ('Good Behavior', .25, 'good');
-INSERT INTO behaviors (name, amount, type) VALUES ('Very Good Behavior', 1, 'good');
-INSERT INTO behaviors (name, amount, type) VALUES ('Bad Behavior', -.25, 'bad');
-INSERT INTO behaviors (name, amount, type) VALUES ('Very Bad Behavior', -1, 'bad');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('Good Behavior', .25, 'good');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('Very Good Behavior', 1, 'good');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('Bad Behavior', -.25, 'bad');
+INSERT OR IGNORE INTO behaviors (name, amount, type) VALUES ('Very Bad Behavior', -1, 'bad');
 
-INSERT INTO chores (name,assigned,time,type) VALUES ('20 minute reading','Evelyn', 20, 'school');
-INSERT INTO chores (name,assigned,time,type) VALUES ('20 minute reading','Virginia', 20, 'school');
-INSERT INTO chores (name,assigned,time,type) VALUES ('10 minute reading','Lucy', 10, 'school');
+INSERT OR IGNORE INTO chores (name,assigned,time,type) VALUES ('20 minute reading','Evelyn', 20, 'school');
+INSERT OR IGNORE INTO chores (name,assigned,time,type) VALUES ('20 minute reading','Virginia', 20, 'school');
+INSERT OR IGNORE INTO chores (name,assigned,time,type) VALUES ('10 minute reading','Lucy', 10, 'school');
 
-INSERT INTO to_do (name,assigned,time,type) VALUES ('20 minute reading','Evelyn', 20, 'school');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('20 minute reading','Virginia', 20, 'school');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('10 minute reading','Lucy', 10, 'school');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Day','Virginia', 10, 'morning');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Day','Evelyn', 10, 'morning');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Day','Lucy', 10, 'morning');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Make it to Bus on Time','Virginia', 5, 'morning');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Make it to Bus on Time','Evelyn', 5, 'morning');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Make it to Bus on Time','Lucy', 5, 'morning');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Bed','Virginia', 5, 'evening');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Bed','Evelyn', 5, 'evening');
-INSERT INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Bed','Lucy', 5, 'evening');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('20 minute reading','Evelyn', 20, 'school');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('20 minute reading','Virginia', 20, 'school');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('10 minute reading','Lucy', 10, 'school');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Day','Virginia', 10, 'morning');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Day','Evelyn', 10, 'morning');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Day','Lucy', 10, 'morning');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Make it to Bus on Time','Virginia', 5, 'morning');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Make it to Bus on Time','Evelyn', 5, 'morning');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Make it to Bus on Time','Lucy', 5, 'morning');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Bed','Virginia', 5, 'evening');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Bed','Evelyn', 5, 'evening');
+INSERT OR IGNORE INTO to_do (name,assigned,time,type) VALUES ('Get Ready For Bed','Lucy', 5, 'evening');
 
-INSERT INTO open_chores (name,time,type) VALUES ('Empty Dishwasher', 5, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Load Dishwasher', 5, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Fold Laundry', 10, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Put Away Laundry', 10, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Tidy Living Room', 5, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Tidy Bedroom', 5, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Clear Table', 3, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Sweep Floor', 10, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Vacuum Floor', 10, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Tidy Boot Bench', 5, 'house');
-INSERT INTO open_chores (name,time,type) VALUES ('Tidy Basement', 10, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Empty Dishwasher', 5, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Load Dishwasher', 5, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Fold Laundry', 10, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Put Away Laundry', 10, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Tidy Living Room', 5, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Tidy Bedroom', 5, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Clear Table', 3, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Sweep Floor', 10, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Vacuum Floor', 10, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Tidy Boot Bench', 5, 'house');
+INSERT OR IGNORE INTO open_chores (name,time,type) VALUES ('Tidy Basement', 10, 'house');
 
 
 
